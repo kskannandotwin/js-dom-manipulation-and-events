c31c757511712ae0ef7dc100394f1518574be035
kskannandotwin <kskannan.win@gmail.com>
2024-01-20 05:14:52 +0530

    20.01.2024-eaehrtd
    completed

--- app.js
+++ app.js
@@ -16,4 +16,19 @@ roundScore = 0;
 activePlayer = 0;
 dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#score-0').textContent = dice;
+document.querySelector("#score-0").textContent = dice;
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  //2. Display the result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+});
